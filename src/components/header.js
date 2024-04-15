dbcaf1a50348ab1b92e551973555e5e1361519f1
jaydemike21 <jaydemike21@gmail.com>
2024-04-15 15:02:08 +0800

    Clean up header

--- src/components/header.js
+++ src/components/header.js
@@ -4,23 +4,26 @@ import './component.css';
 
 export default function Header() {
     return(
-        <div className='headerContainer'>
-            <div className='headerLeft'>
+        <div className='header'>
+
+            <nav>
                 <ul>
-                    <li><a href='#'>Logo</a></li>
+                    <li><a href='/'><img src='https://starbucks.ph/images/sblogo.svg' alt='starbucks logo' width={"50px"}  /></a></li>
                     <li><a href='#'>Menu</a></li>
                     <li><a href='#'>Rewards</a></li>
                     <li><a href='#'>Gift Cards</a></li>
                 </ul>
+            </nav>
+            
+            <div className='rightHeader'>
+                <ul>
+                    <li><a href='' className='storebtn'>Find a store</a></li>
+                    <li><a href='' className='signbtn'>Sign In</a></li>
+                    <li><a href='' className='joinbtn'>Join Now</a></li>
+
+                </ul>   
             </div>
 
-            <div className='headerRight'>
-                <ul>
-                    <li><a href='#' className='store'>Find a store</a></li>
-                    <li><a href='#' className='signin'>Sign in</a></li>
-                    <li><a href='#' className='join'>Join now</a></li>
-                </ul>
-            </div>            
         </div>
     )
 }
